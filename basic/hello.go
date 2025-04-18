91b9a259ca3044b83392ae33e01bd3689340e3b4
shivam <shivam7.dev@gmail.com>
2025-04-18 12:14:52 +0530

    first go program

---
+++ basic/hello.go
@@ -0,0 +1,13 @@
+package main //package declaration
+
+import "fmt" //Imported package
+
+func main() { //main funtion (entry point)
+	fmt.Println("hello from go ")
+}
+
+/*
+*Key points
+*Every file must have a package declartion
+*the main package should have a main funtion
+ */
