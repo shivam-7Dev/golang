edd5017696f733fde1c9b34439e1250d2a1fe296
shivam <shivam7.dev@gmail.com>
2025-04-18 16:00:57 +0530

    added some files

--- basic/hello.go
+++ basic/hello.go
@@ -10,4 +10,7 @@ func main() { //main funtion (entry point)
 *Key points
 *Every file must have a package declartion
 *the main package should have a main funtion
+
+* to run a file => go run file.go
+* to build a file => go build file.go
  */
